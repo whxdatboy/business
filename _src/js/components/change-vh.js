4bfa7ec3b9811490cfa678d6be20372d3cf3b268
Roman <klygin.r@cadesign.ru>
2021-10-15 17:59:59 +0300

    памагити(

--- _src/js/components/change-vh.js
+++ _src/js/components/change-vh.js
@@ -1,4 +1,10 @@
+window.addEventListener('resize', function() {
 if (window.innerWidth <= 1024) {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  if(window.innerWidth <= 1024 && window.innerHeight <= 1236) {
+    document.querySelector('.main-slider__img-wrapper').style.display = 'none';
+  }
+})
--- business-theorema/js/script.js
+++ business-theorema/js/script.js
@@ -72,10 +72,16 @@ function bulletChange() {}
   \*****************************************/
 /***/ (() => {
 
-if (window.innerWidth <= 1024) {
-  var vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
-}
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 1024) {
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
+  }
+
+  if (window.innerWidth <= 1024 && window.innerHeight <= 1236) {
+    document.querySelector('.main-slider__img-wrapper').style.display = 'none';
+  }
+});
 
 /***/ }),
 
